c48993999d094c72eb4f4226d85adc02f6ed3fb5
a l C o P a U L <paul.amurao@gmail.com>
2018-12-28 07:49:36 -0500

    Update Module.cpp

--- Module.cpp
+++ Module.cpp
@@ -66,10 +66,10 @@ int main()
 	struct tm tm;
 	localtime_s(&tm, &t);
 	// welcome text
-	printf("New York Lottery v4.7 by alcopaul (twitter: @thealcopaul)\n\n");
+	printf("New York Lottery v4.8 by alcopaul (twitter: @thealcopaul)\n\n");
 	printf("#########################################################\n\n");
 	// day as seed
-	srand((tm.tm_mon + 2) + (tm.tm_mday - 1) + (tm.tm_year + 1900));
+	srand((tm.tm_mon + 1) + (tm.tm_mday) * (tm.tm_year + 1900));
 	for (int i = 1; i < 7; i++)
 	{
 		switch (i)
@@ -131,4 +131,4 @@ int main()
 	char c = getchar();
 	return 0;
 }
-// New York Lottery v4.7 by alcopaul (twitter: @thealcopaul)
+// New York Lottery v4.8 by alcopaul (twitter: @thealcopaul)
